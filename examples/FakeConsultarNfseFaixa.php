f8d51d7c72fbfe35591404b8efeb9ff21d313dd2
robmachado <linux.rlm@gmail.com>
2020-03-13 10:07:31 -0300

    importação e complementação de ctodobom/sped-nfse-publica

---
+++ examples/FakeConsultarNfseFaixa.php
@@ -0,0 +1,43 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+require_once '../bootstrap.php';
+
+use NFePHP\Common\Certificate;
+use NFePHP\NFSePublica\Tools;
+use NFePHP\NFSePublica\Common\Soap\SoapFake;
+use NFePHP\NFSePublica\Common\FakePretty;
+
+try {
+    
+    $config = [
+        'cnpj' => '99999999000191',
+        'im' => '1733160024',
+        'cmun' => '4204202', //ira determinar as urls e outros dados
+        'razao' => 'Empresa Test Ltda',
+        'tpamb' => 2
+    ];
+
+    $configJson = json_encode($config);
+
+    $content = file_get_contents('expired_certificate.pfx');
+    $password = 'associacao';
+    $cert = Certificate::readPfx($content, $password);
+    
+    $soap = new SoapFake();
+    $soap->disableCertValidation(true);
+    
+    $tools = new Tools($configJson, $cert);
+    $tools->loadSoapClass($soap);
+
+    $nano = 2018;
+    $nini = '1';
+    $nfim = '100';
+
+    $response = $tools->consultarNfseFaixa($nini, $nfim, $nano);
+    
+    echo FakePretty::prettyPrint($response, '');
+ 
+} catch (\Exception $e) {
+    echo $e->getMessage();
+}
